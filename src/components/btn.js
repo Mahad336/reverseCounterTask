1c64e8563771986f5e7dacfc5d436042e12ee8b7
Mahad Younis <younismahad123@gmail.com>
2022-09-24 00:09:17 +0500

    a bit style

--- src/components/btn.js
+++ src/components/btn.js
@@ -2,7 +2,19 @@ import React from "react";
 
 const Bttn = (props) => {
   return (
-    <div style={{ marginLeft: 5 }}>
+    <div
+      className="btns"
+      style={{
+        fontSize: 50,
+        justifyContent: "center",
+        alignItems: "center",
+        display: "flex",
+        backgroundColor: "Black",
+        marginLeft: 600,
+        marginRight: 600,
+        borderRadius: 30,
+      }}
+    >
       {props.status === 0 ? <button onClick={props.start}> Start </button> : ""}
 
       {props.status === 1 ? <button onClick={props.stop}>Stop</button> : ""}
